9703ab68a3a56e1cc1ef76f05a8d139be60aa67c
jucasoft <juca@jucasoft.com>
2019-10-10 12:54:00 +0200

    changed router-store

--- src/app/root-store/router-store/index.ts
+++ src/app/root-store/router-store/index.ts
@@ -2,11 +2,10 @@ import * as RouterStoreActions from './actions';
 import * as RouterStoreSelectors from './selectors';
 
 export {
-    RouterStoreModule
+  RouterStoreModule
 } from './router-store.module';
 
 export {
-    RouterStoreActions,
-    RouterStoreSelectors
+  RouterStoreActions,
+  RouterStoreSelectors
 };
-
